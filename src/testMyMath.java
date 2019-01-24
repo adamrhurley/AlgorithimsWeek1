717db63034b23ebf5ff408f7c2b9b8c9dc744e0b
t00198396 <adam.hurley@students.ittralee.ie>
2019-01-24 17:21:15 +0000

    Week1

---
+++ src/testMyMath.java
@@ -0,0 +1,40 @@
+public class testMyMath {
+    public static void main(String[] args) {
+      /* int result =  MyMath.findSmallest(1, 2, 3);
+         System.out.println(result); //Expect 1
+
+        result =  MyMath.findSmallest(43, 12, 58);
+        System.out.println(result);//Expect 12
+
+        result =  MyMath.findSmallest(53, 12, 9);
+        System.out.println(result);//Expect 9
+
+
+        /////////////////////////////////
+
+        int result2 = MyMath.findPower(10, 4);
+                System.out.println(result2); //10^4 Expect 10,000
+
+        result2 = MyMath.findPower(2, 5);
+        System.out.println(result2); //2^5 Expect 32
+
+        result2 = MyMath.findPower(0, 5);
+        System.out.println(result2); //0^5 Expect 0
+
+        result2 = MyMath.findPower(3, 0);
+        System.out.println(result2); //3^0 Expect 1*/
+
+      int result3 = MyMath.sumNumbers(1);
+      System.out.println(result3);//Expect 1
+
+         result3 = MyMath.sumNumbers(2);
+        System.out.println(result3);//Expect 3
+
+        result3 = MyMath.sumNumbers(3);
+        System.out.println(result3);//Expect 6
+
+        result3 = MyMath.sumNumbers(4);
+        System.out.println(result3);//Expect 10
+
+    }
+}
