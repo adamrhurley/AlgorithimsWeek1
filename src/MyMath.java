42fb3308080105ec94c3846dacffabc2bcd34887
Adam Hurley <43179127+adamrhurley@users.noreply.github.com>
2019-01-31 17:02:02 +0000

    Add files via upload

---
+++ src/MyMath.java
@@ -0,0 +1,58 @@
+public class MyMath {
+    public static int findSmallest(int x, int y, int z)
+    {
+        int smallest;
+        smallest = x;
+
+        if (y < smallest)
+            smallest = y;
+
+        if (z < smallest)
+            smallest = z;
+
+        return smallest;
+    }
+
+    public static int findPower(int x, int y)
+    {
+        int result = 1;
+
+        if (x < 0)
+           result = 0;
+
+        for(int i=0;i < y; i++)
+            result *= x;
+
+        return result;
+    }
+
+    public static int sumNumbers(int n)
+    {
+        int result = 0;
+        int num = 0;
+        for(int i=0;i < n; i++) {
+            num++;
+            result += num;
+        }
+        return result;
+    }
+
+    public static int factorial(int n)
+    {
+        int result = 1;
+        int num = 0;
+        for(int i=0;i < n; i++) {
+            num++;
+            result *= num;
+        }
+        return result;
+    }
+
+    public static int primeNum(int n)
+    {
+
+     return n;
+    }
+
+
+}
---
+++ testMyMath.java
@@ -0,0 +1,52 @@
+public class testMyMath {
+    public static void main(String[] args) {
+      /* int result =  MyMath.findSmallest(1, 2, 3);
+         System.out.println(result); //Expect 1
+
+        result =  MyMath.findSmallest(43, 12, 58);
+        System.out.println(result);//Expect 12
+
+        result =  MyMath.findSmallest(53, 12, 9);
+        System.out.println(result);//Expect 9
+
+
+        /////////////////////////////////
+
+        int result2 = MyMath.findPower(10, 4);
+                System.out.println(result2); //10^4 Expect 10,000
+
+        result2 = MyMath.findPower(2, 5);
+        System.out.println(result2); //2^5 Expect 32
+
+        result2 = MyMath.findPower(0, 5);
+        System.out.println(result2); //0^5 Expect 0
+
+        result2 = MyMath.findPower(3, 0);
+        System.out.println(result2); //3^0 Expect 1
+
+      int result3 = MyMath.sumNumbers(1);
+      System.out.println(result3);//Expect 1
+
+         result3 = MyMath.sumNumbers(2);
+        System.out.println(result3);//Expect 3
+
+        result3 = MyMath.sumNumbers(3);
+        System.out.println(result3);//Expect 6
+
+        result3 = MyMath.sumNumbers(4);
+        System.out.println(result3);//Expect 10
+
+      int result4 = MyMath.factorial(3);
+      System.out.println(result4);//Expect 6
+
+        result4 = MyMath.factorial(6);
+        System.out.println(result4);//Expect 720
+
+        result4 = MyMath.factorial(17);
+        System.out.println(result4);//Expect 3628800*/
+
+      int result5 = MyMath.primeNum(7);
+      System.out.println(result5);
+
+    }
+}
